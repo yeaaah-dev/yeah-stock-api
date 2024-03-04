bb9f4fb17809f4ae48a34b8d69765d5363cbab6f
Yan-pg <yancesa.07@gmail.com>
2024-03-04 19:51:00 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,40 @@
 import "reflect-metadata";
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-import 'express-async-errors';
+import "express-async-errors";
 import "./database/data-source";
 import { AppError } from "./shared/errors/AppError";
 import { routes } from "./routes/routes";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
     console.log(error);
-    
+
     if (error instanceof AppError) {
-        return response.status(error.status).send({
-            status: 'error',
-            message: error.message
-        });
+      return response.status(error.status).send({
+        status: "error",
+        message: error.message,
+      });
     }
 
     return response.status(500).send({
-        status: 'error',
-        message: 'Internal Server Error!'
+      status: "error",
+      message: "Internal Server Error!",
     });
+  }
+);
+
+app.get("/", (request, response) => {
+  return response.status(200).send("Hello World!");
 });
 
-app.get('/', (request, response)=> {
-    return response.status(200).send("Hello World!");
-})
-
 app.listen(3333, () => {
-    console.log("App started at port 3333!")
+  console.log("App started at port 3333!");
 });
